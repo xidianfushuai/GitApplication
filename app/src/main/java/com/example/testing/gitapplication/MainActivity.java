9866498094fbff18bec40b1e9885c73d5b8d8806
fushuai <fus@hui10.com>
2017-06-30 15:57:03 +0800

    cahnge Mainactivity hello to hello world

--- app/src/main/java/com/example/testing/gitapplication/MainActivity.java
+++ app/src/main/java/com/example/testing/gitapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show();
     }
 }
